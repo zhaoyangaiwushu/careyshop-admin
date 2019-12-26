8d204543d0cc6c695b06114e82cd52cbedfba1e3
思维空间 <dnyz520@126.com>
2019-12-26 17:02:46 +0800

    up

--- src/views/goods/admin/update.vue
+++ src/views/goods/admin/update.vue
@@ -8,6 +8,7 @@
       :cat-data="catData"
       :brand-data="brandData"
       :type-data="typeData"
+      :buffer-data="pageData"
       @close="handleClose">
     </page-edit>
 
@@ -30,6 +31,11 @@
 
 <script>
 import { mapActions } from 'vuex'
+import { getBrandSelect } from '@/api/goods/brand'
+import { getGoodsCategoryList } from '@/api/goods/category'
+import { getGoodsTypeSelect } from '@/api/goods/type'
+import { getGoodsAttrConfig, getGoodsItem, getGoodsSpecConfig } from '@/api/goods/goods'
+import util from '@/utils/util'
 
 export default {
   name: 'goods-admin-update',
@@ -48,7 +54,51 @@ export default {
       confirmLoading: false,
       catData: [],
       brandData: [],
-      typeData: []
+      typeData: [],
+      // 数据缓存
+      buffer: {},
+      // 组件内部数据
+      pageData: {},
+      // 判断是否路由进入
+      isSourceRoute: false
+    }
+  },
+  mounted() {
+    Promise.all([
+      getBrandSelect({ order_field: 'phonetic' }),
+      getGoodsTypeSelect({ order_type: 'asc' }),
+      getGoodsCategoryList(null)
+    ])
+      .then(res => {
+        this.brandData = res[0].data || []
+        this.typeData = res[1].data || []
+        this.catData = util.formatDataToTree(res[2].data, 'goods_category_id')
+
+        this.$nextTick(() => {
+          if (!this.isSourceRoute && !Object.keys(this.buffer).length) {
+            this.switchData(this.goods_id)
+          }
+        })
+      })
+  },
+  // 第一次进入或从其他组件对应路由进入时触发
+  beforeRouteEnter(to, from, next) {
+    if (to.params.goods_id) {
+      next(vm => {
+        vm.switchData(to.params.goods_id)
+        vm.isSourceRoute = true
+      })
+    } else {
+      next(new Error('未指定ID'))
+    }
+  },
+  // 在同一组件对应的多个路由间切换时触发
+  beforeRouteUpdate(to, from, next) {
+    if (to.params.goods_id) {
+      this.switchData(to.params.goods_id)
+      next()
+    } else {
+      next(new Error('未指定ID'))
     }
   },
   methods: {
@@ -61,9 +111,61 @@ export default {
     },
     // 关闭当前窗口
     handleClose() {
-      this.close({
-        tagName: this.$route.fullPath
-      })
+      console.log(this.$refs.update)
+      // console.log(this.buffer)
+      // this.close({
+      //   tagName: this.$route.fullPath
+      // })
+    },
+    switchData(id) {
+      /**
+       * 1.检测缓存是否存在
+       * 2.存在则直接返回
+       * 3.不存在进行关联
+       */
+
+      // // 等待子组件创建完成
+      // let sleep = true
+      // while (sleep) {
+      //   setTimeout((sleep) => {
+      //     if (this.$refs.update) {
+      //       sleep = false
+      //     }
+      //
+      //     console.log('okok')
+      //   }, 1000)
+      // }
+      //
+      // if (this.buffer[id]) {
+      //   // 存在缓存时切换后可直接返回
+      //   this.pageData = this.buffer[id] || {}
+      // } else {
+      //   // 组件缓存不存在时进行绑定
+      //   this.buffer[id] = this.$refs.update.$data
+      // }
+
+      // // 否则请求获取数据
+      // this.$nextTick(() => {
+      //   this.loading = true
+      // })
+      //
+      // if (this.isInitial) {
+      //   // 已初始化
+      //   Promise.all([
+      //     getGoodsItem(id),
+      //     getGoodsAttrConfig(id),
+      //     getGoodsSpecConfig(id, 1)
+      //   ])
+      //     .then(res => {
+      //       tihs.pageData = {
+      //       }
+      //     })
+      //     .finally(() => {
+      //       this.$nextTick(() => {
+      //         this.loading = false
+      //       })
+      //     })
+      // }
     }
   }
 }

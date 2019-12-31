85c1d2a85fe110b9c8a12934718c462235b8597d
思维空间 <dnyz520@126.com>
2019-12-31 17:10:38 +0800

    no message

--- src/views/goods/admin/view.vue
+++ src/views/goods/admin/view.vue
@@ -15,6 +15,36 @@
           </el-tab-pane>
 
           <el-tab-pane label="商品属性" name="attr">
+            <el-collapse v-model="attrActive">
+              <form class="el-form el-form--label-right">
+                <el-collapse-item
+                  v-for="(value, index) in attrConfig"
+                  :key="index"
+                  :name="value.goods_attribute_id"
+                  :title="value.attr_name">
+                  <div
+                    v-for="item in value.get_attribute"
+                    :key="item.goods_attribute_id"
+                    class="el-form-item attr-form">
+                    <label class="el-form-item__label attr-label">
+                      <span :title="item.attr_name">{{item.attr_name}}</span>
+                    </label>
+
+                    <div class="el-form-item__content attr-content">
+                      <div class="attr-description">
+                        <el-tooltip
+                          v-if="item.description"
+                          :content="item.description"
+                          placement="top">
+                          <i class="el-icon-warning-outline"/>
+                        </el-tooltip>
+                      </div>
+                      <span>{{item.attr_value}}</span>
+                    </div>
+                  </div>
+                </el-collapse-item>
+              </form>
+            </el-collapse>
           </el-tab-pane>
         </el-tabs>
       </el-card>
@@ -23,6 +53,9 @@
 </template>
 
 <script>
+import { getGoodsAttrList, getGoodsItem, getGoodsSpecMenu } from '@/api/goods/goods'
+import { getGoodsAttributeData } from '@/api/goods/attribute'
+
 export default {
   props: {
     goods_id: {
@@ -42,17 +75,84 @@ export default {
     return {
       loading: true,
       activeName: 'content',
-      goodsData: {}
+      goodsData: {},
+      attrActive: [],
+      attrConfig: [],
+      attrImportant: [],
+      specCombo: [],
+      specConfig: []
     }
   },
   methods: {
     resetGoodsData() {
       this.activeName = 'content'
       this.goodsData = {}
+      this.attrActive = []
+      this.attrConfig = []
+      this.attrImportant = []
+      this.specCombo = []
+      this.specConfig = []
+    },
+    getGoodsOtherInfo() {
+      Promise.all([
+        getGoodsSpecMenu(this.goods_id),
+        getGoodsAttrList(this.goods_id),
+        getGoodsAttributeData(this.goodsData.goods_type_id, 1)
+      ])
+        .then(res => {
+          this.specCombo = res[0].data['spec_combo'] || []
+          this.specConfig = res[0].data['spec_config'] || []
+
+          let key = {}
+          for (let value of res[1].data) {
+            if (!value.attr_value) {
+              continue
+            }
+
+            if (key[value.parent_id] === undefined) {
+              const parentData = res[2].data[value.parent_id]
+              if (!parentData) {
+                continue
+              }
+
+              let pos = this.attrConfig.push({
+                goods_attribute_id: parentData['goods_attribute_id'],
+                attr_name: parentData['attr_name'],
+                get_attribute: []
+              })
+
+              key[value.parent_id] = pos - 1
+              this.attrActive.push(value.parent_id)
+            }
+
+            const attrData = res[2].data[value.goods_attribute_id]
+            if (value.is_important) {
+              this.attrImportant.push({
+                attr_name: attrData['attr_name'],
+                attr_value: value.attr_value
+              })
+            }
+
+            this.attrConfig[key[value.parent_id]].get_attribute.push({
+              description: attrData['description'],
+              attr_name: attrData['attr_name'],
+              attr_value: value.attr_value
+            })
+          }
+        })
     },
     getGoodsData() {
       this.loading = true
       this.resetGoodsData()
+
+      getGoodsItem(this.goods_id)
+        .then(res => {
+          this.goodsData = res.data || {}
+          this.getGoodsOtherInfo()
+        })
+        .finally(() => {
+          this.loading = false
+        })
     }
   }
 }
@@ -80,5 +180,23 @@ export default {
   }
   .clearfix:after {
     clear: both
+  }
+  .attr-form {
+    padding-left: 15px;
+    margin-bottom: 0;
+  }
+  .attr-label {
+    width: 140px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    color: $color-info;
+  }
+  .attr-description {
+    display: inline-block;
+    width: 24px;
+  }
+  .attr-content {
+    margin-left: 140px;
   }
 </style>

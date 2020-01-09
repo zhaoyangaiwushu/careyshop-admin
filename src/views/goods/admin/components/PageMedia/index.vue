8a81b917262fc0e13439aefa4c61b8a18ad54e97
思维空间 <dnyz520@126.com>
2020-01-09 16:00:02 +0800

    no message

--- src/views/goods/admin/components/PageMedia/index.vue
+++ src/views/goods/admin/components/PageMedia/index.vue
@@ -1,28 +1,49 @@
 <template>
   <div class="magnify">
     <!-- 左侧容器 -->
-    <div class="left_contaner">
-      <div class="middle_img">
-<!--        <el-image-->
-<!--          :src="currentImage | getPreviewUrl('goods_image_x480')"-->
-<!--          :style="{width: width + 'px', height: height + 'px'}"-->
-<!--          fit="contain">-->
-<!--          <div slot="placeholder" class="image-slot">-->
-<!--          </div>-->
-<!--          <div slot="error" class="image-slot">-->
-<!--          </div>-->
-<!--        </el-image>-->
+    <div class="left-contaner">
+      <div
+        :class="{'middle-img': true, 'video-bg': isVideoPlay}"
+        :style="{width: width + 'px', height: height + 'px'}">
+        <i
+          v-if="!isVideoEmpty && !isVideoPlay"
+          class="el-icon-video-play video-button play"
+          @click="isVideoPlay = true"/>
+
+        <template v-if="isVideoPlay">
+          <i
+            class="el-icon-circle-close video-button stop"
+            @click="isVideoPlay = false"/>
+
+          <cs-video
+            class="video-box"
+            :style="{width: width + 'px'}"
+            :video-data="video"
+            :autoplay="true">
+          </cs-video>
+        </template>
+
+        <img
+          v-show="!isVideoPlay"
+          :src="currentImage | getPreviewUrl('goods_image_x480')"
+          :style="{
+            'width': 'auto',
+            'height': 'auto',
+            'max-width': (width - 2) + 'px',
+            'max-height': (height - 2) + 'px'
+          }"
+          alt="">
       </div>
 
       <!-- 缩略图集 -->
       <div class="carousel" :style="{width: width + 'px', height: thumbHeight + 'px'}">
         <i class="el-icon-arrow-left arrow" @click="clickPage('left')"/>
 
-        <div class="show_box">
-          <ul class="picture_container" :style="{left: middleLeft + 'px'}">
+        <div class="show-box">
+          <ul class="picture-container" :style="{left: middleLeft + 'px'}">
             <li
               v-for="(item, index) in imageList"
-              class="picture_item"
+              class="picture-item"
               :class="{selected: currentIndex === index}"
               :key="index"
               @mouseover="tabPicture(index)"
@@ -39,7 +60,7 @@
                   'max-width': (thumbWidth - 4) + 'px',
                   'max-height': (thumbHeight - 4) + 'px'
                 }"
-                alt=""/>
+                alt="">
             </li>
           </ul>
         </div>
@@ -56,9 +77,12 @@
 
 <script>
 import util from '@/utils/util'
-import { debounce } from 'lodash'
+import { debounce, isEmpty } from 'lodash'
 
 export default {
+  components: {
+    'csVideo': () => import('@/components/cs-video')
+  },
   props: {
     image: {
       type: Array,
@@ -100,7 +124,8 @@ export default {
       imageList: [],
       middleLeft: 0,
       currentImage: {},
-      currentIndex: undefined
+      currentIndex: undefined,
+      isVideoPlay: false
     }
   },
   filters: {
@@ -126,6 +151,9 @@ export default {
   computed: {
     itemMargin() {
       return (this.width - (this.thumbWidth * this.pageSize) - 51) / this.pageSize
+    },
+    isVideoEmpty() {
+      return isEmpty(this.video)
     }
   },
   methods: {
@@ -134,6 +162,7 @@ export default {
       this.middleLeft = 0
       this.currentIndex = undefined
       this.currentImage = this.imageList[0]
+      this.isVideoPlay = false
     },
     // 插入图集到列表中
     updateImage(imageList = []) {
@@ -170,13 +199,13 @@ export default {
   .magnify {
     position: relative;
   }
-  .left_contaner {
+  .left-contaner {
     width: 100%;
     height: 100%;
     .carousel {
       margin-top: 20px;
       display: inline-flex;
-      .show_box {
+      .show-box {
         flex: 1;
         overflow: hidden;
         position: relative;
@@ -193,12 +222,12 @@ export default {
         flex-basis: 25px;
         color: $color-border-1;
       }
-      .picture_container {
+      .picture-container {
         position: absolute;
         overflow: hidden;
         top: 0;
         left: 0;
-        .picture_item {
+        .picture-item {
           @extend %flex-center-row;
           float: left;
           box-sizing: border-box;
@@ -208,6 +237,34 @@ export default {
             border-color: $color-danger;
           }
         }
+      }
+    }
+    .middle-img {
+      @extend %flex-center-row;
+      border: 1px solid $color-border-1;
+      box-sizing: border-box;
+      position: relative;
+      overflow: hidden;
+      &.video-bg {
+        background: #000000;
+      }
+      .video-button {
+        @extend %unable-select;
+        font-size: 38px;
+        color: $color-text-normal;
+        position: absolute;
+        &.play {
+          bottom: 10px;
+          left: 10px;
+        }
+        &.stop {
+          top: 10px;
+          right: 10px;
+        }
+      }
+      .video-box {
+        position: absolute;
+        bottom: 0;
       }
     }
   }

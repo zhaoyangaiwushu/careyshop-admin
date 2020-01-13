6f7eed0ff1e59f0beaf762c5b117847117707dd0
思维空间 <dnyz520@126.com>
2020-01-13 16:14:23 +0800

    no message

--- src/components/cs-goods-select/index.vue
+++ src/components/cs-goods-select/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
-  </div>
+  <el-dialog
+    title="商品选取"
+    :visible.sync="visible"
+    :append-to-body="true"
+    :close-on-click-modal="false"
+    width="700px">
+  </el-dialog>
 </template>
 
 <script>
 export default {
-  name: 'cs-goods-select'
+  name: 'cs-goods-select',
+  // components: {
+  //   'PageFooter': () => import('@/layout/header-aside/components/footer')
+  // },
+  props: {
+  },
+  data() {
+    return {
+      visible: false,
+      loading: true,
+      confirmLoading: false
+    }
+  }
 }
 </script>

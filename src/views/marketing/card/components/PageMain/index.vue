96602c61f8a157fa9bd0a40329d72287fbc6afa1
思维空间 <dnyz520@126.com>
2020-01-20 10:35:10 +0800

    no message

--- src/views/marketing/card/components/PageMain/index.vue
+++ src/views/marketing/card/components/PageMain/index.vue
@@ -7,13 +7,141 @@
         <el-button
           icon="el-icon-plus"
           :disabled="loading"
-          @click="() => {}">新增购物卡</el-button>
+          @click="handleCreate">新增购物卡</el-button>
       </el-form-item>
+
+      <el-form-item>
+        <el-button-group>
+          <el-button
+            icon="el-icon-check"
+            :disabled="loading"
+            @click="handleStatus(null, 1, true)">启用</el-button>
+
+          <el-button
+            icon="el-icon-close"
+            :disabled="loading"
+            @click="handleStatus(null, 0, true)">禁用</el-button>
+        </el-button-group>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button
+          icon="el-icon-delete"
+          :disabled="loading"
+          @click="handleDelete(null)">删除</el-button>
+      </el-form-item>
+
+      <cs-help
+        :router="$route.path"
+        style="padding-bottom: 19px;">
+      </cs-help>
     </el-form>
+
+    <el-table
+      v-loading="loading"
+      :data="currentTableData"
+      :highlight-current-row="true"
+      @selection-change="handleSelectionChange"
+      @sort-change="sortChange">
+      <el-table-column align="center" type="selection" width="50"/>
+
+      <el-table-column
+        label="名称"
+        prop="name"
+        sortable="custom"
+        min-width="100"
+        :show-overflow-tooltip="true">
+        <template slot-scope="scope">
+          <el-tooltip
+            v-if="scope.row.description"
+            :content="`描述：${scope.row.description}`"
+            placement="top-start">
+            <i class="el-icon-tickets"/>
+          </el-tooltip>
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="面额"
+        sortable="custom"
+        prop="money">
+        <template slot-scope="scope">
+          {{scope.row.money | getNumber}}
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="发放数"
+        sortable="custom"
+        prop="give_num">
+      </el-table-column>
+
+      <el-table-column
+        label="激活数"
+        sortable="custom"
+        prop="active_num">
+      </el-table-column>
+
+      <el-table-column
+        label="日期"
+        align="center">
+        <template slot-scope="scope">
+          <el-tooltip placement="top">
+            <div slot="content">
+              创建日期：{{scope.row.create_time}}<br/>
+              有效日期：{{scope.row.end_time}}
+            </div>
+            <el-tag size="mini" effect="plain" type="info">详细</el-tag>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="状态"
+        prop="status"
+        sortable="custom"
+        align="center"
+        width="100">
+        <template slot-scope="scope">
+          <el-tag
+            size="mini"
+            :type="statusMap[scope.row.status].type"
+            :style="auth.enable || auth.disable ? 'cursor: pointer;' : ''"
+            :effect="auth.enable || auth.disable ? 'light' : 'plain'"
+            @click.native="handleStatus(scope.$index)">
+            {{statusMap[scope.row.status].text}}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="操作"
+        align="center"
+        min-width="100">
+        <template slot-scope="scope">
+          <el-button
+            @click="handleUpdate(scope.$index)"
+            size="small"
+            type="text">编辑</el-button>
+
+          <el-button
+            @click="handleDelete(scope.$index)"
+            size="small"
+            type="text">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import {
+  delCardList,
+  setCardStatus
+} from '@/api/marketing/card'
+import util from '@/utils/util'
+
 export default {
   props: {
     loading: {
@@ -52,8 +180,8 @@ export default {
         add: false,
         set: false,
         del: false,
-        enable: false,
-        disable: false
+        enable: true,
+        disable: true
       },
       form: {
         name: undefined,
@@ -68,9 +196,156 @@ export default {
       rules: {
       }
     }
+  },
+  filters: {
+    getNumber(val) {
+      return util.getNumber(val)
+    }
+  },
+  watch: {
+    tableData: {
+      handler(val) {
+        this.currentTableData = val
+      },
+      immediate: true
+    }
+  },
+  mounted() {
+    this._validationAuth()
+  },
+  methods: {
+    // 验证权限
+    _validationAuth() {
+    },
+    // 获取列表中的编号
+    _getIdList(val) {
+      if (val === null) {
+        val = this.multipleSelection
+      }
+
+      let idList = []
+      if (Array.isArray(val)) {
+        val.forEach(value => {
+          idList.push(value.card_id)
+        })
+      } else {
+        idList.push(this.currentTableData[val].card_id)
+      }
+
+      return idList
+    },
+    // 选中数据项
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    // 获取排序字段
+    sortChange({ column, prop, order }) {
+      let sort = {
+        order_type: undefined,
+        order_field: undefined
+      }
+
+      if (column && order) {
+        sort.order_type = order === 'ascending' ? 'asc' : 'desc'
+        sort.order_field = prop
+      }
+
+      this.$emit('sort', sort)
+    },
+    // 批量设置状态
+    handleStatus(val, status = 0, confirm = false) {
+      let card_id = this._getIdList(val)
+      if (card_id.length === 0) {
+        this.$message.error('请选择要操作的数据')
+        return
+      }
+
+      function setStatus(card_id, status, vm) {
+        setCardStatus(card_id, status)
+          .then(() => {
+            vm.currentTableData.forEach((value, index) => {
+              if (card_id.indexOf(value.card_id) !== -1) {
+                vm.$set(vm.currentTableData, index, {
+                  ...value,
+                  status
+                })
+              }
+            })
+
+            vm.$message.success('操作成功')
+          })
+      }
+
+      if (!confirm) {
+        let oldData = this.currentTableData[val]
+        const newStatus = oldData.status ? 0 : 1
+
+        if (oldData.status > 1) {
+          return
+        }
+
+        // 禁用权限检测
+        if (newStatus === 0 && !this.auth.disable) {
+          return
+        }
+
+        // 启用权限检测
+        if (newStatus === 1 && !this.auth.enable) {
+          return
+        }
+
+        this.$set(this.currentTableData, val, { ...oldData, status: 2 })
+        setStatus(card_id, newStatus, this)
+        return
+      }
+
+      this.$confirm('确定要执行该操作吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        closeOnClickModal: false
+      })
+        .then(() => {
+          setStatus(card_id, status, this)
+        })
+        .catch(() => {
+        })
+    },
+    // 批量删除
+    handleDelete(val) {
+      let card_id = this._getIdList(val)
+      if (card_id.length === 0) {
+        this.$message.error('请选择要操作的数据')
+        return
+      }
+
+      this.$confirm('确定要执行该操作吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        closeOnClickModal: false
+      })
+        .then(() => {
+          delCardList(card_id)
+            .then(() => {
+              for (let i = this.currentTableData.length - 1; i >= 0; i--) {
+                if (card_id.indexOf(this.currentTableData[i].card_id) !== -1) {
+                  this.currentTableData.splice(i, 1)
+                }
+              }
+
+              this.$message.success('操作成功')
+            })
+        })
+        .catch(() => {
+        })
+    },
+    // 弹出新建对话框
+    handleCreate() {
+    },
+    // 弹出编辑对话框
+    handleUpdate(index) {
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>

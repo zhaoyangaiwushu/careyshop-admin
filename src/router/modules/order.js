427464d544291535f0cbdb229ab96ebc0e849af4
思维空间 <dnyz520@126.com>
2020-02-13 18:00:20 +0800

    no message

--- src/router/modules/order.js
+++ src/router/modules/order.js
@@ -11,6 +11,12 @@ export default {
       name: `${pre}index`,
       meta: { ...meta, cache: true, title: '订单首页' },
       component: () => import('@/views/order/index')
+    },
+    {
+      path: 'admin/refund',
+      name: `${pre}admin-refund`,
+      meta: { ...meta, cache: true, title: '退款日志' },
+      component: () => import('@/views/order/refund')
     }
   ])('order-')
 }

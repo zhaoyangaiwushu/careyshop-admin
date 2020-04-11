350d260198a18a4fada24a5f8f4cfa74f30a849b
思维空间 <dnyz520@126.com>
2020-04-11 23:34:07 +0800

    up

---
+++ webpack.conf.js
@@ -0,0 +1,13 @@
+// 拼接路径
+const resolve = dir => require('path').join(__dirname, dir)
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+      '@static': resolve('public/static')
+    }
+  }
+}

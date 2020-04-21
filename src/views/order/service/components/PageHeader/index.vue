46df360d7d837550f1d7a27e343bb910570faa81
思维空间 <dnyz520@126.com>
2020-04-21 16:49:22 +0800

    no message

--- src/views/order/service/components/PageHeader/index.vue
+++ src/views/order/service/components/PageHeader/index.vue
@@ -1,13 +1,167 @@
 <template>
+  <el-form
+    :inline="true"
+    :model="form"
+    ref="form"
+    size="mini"
+    style="margin-bottom: -18px;">
+    <el-form-item label="流水号" prop="order_code">
+      <el-input
+        v-model="form.order_code"
+        prefix-icon="el-icon-search"
+        placeholder="可输入售后单号/订单号"
+        @keyup.enter.native="handleFormSubmit(true)"
+        :clearable="true"/>
+    </el-form-item>
 
+    <el-form-item label="账号" prop="account">
+      <el-input
+        v-model="form.account"
+        prefix-icon="el-icon-search"
+        placeholder="可输入账号/昵称"
+        @keyup.enter.native="handleFormSubmit(true)"
+        :clearable="true"/>
+    </el-form-item>
+
+    <el-form-item label="状态" prop="status">
+      <el-select
+        v-model="form.status"
+        placeholder="请选择"
+        style="width: 120px;"
+        clearable>
+        <el-option
+          v-for="(item, index) in statusMap"
+          :key="index"
+          :label="item"
+          :value="index"/>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        type="primary"
+        icon="el-icon-search"
+        :disabled="loading"
+        @click="handleFormSubmit(true)">查询</el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        icon="el-icon-refresh"
+        @click="handleFormReset">重置</el-button>
+    </el-form-item>
+
+    <el-form-item>
+      <el-popover
+        width="397"
+        placement="bottom"
+        trigger="click">
+        <div class="more-filter">
+          <el-form-item label="售后类型" prop="type">
+            <el-select
+              v-model="form.type"
+              placeholder="请选择"
+              style="width: 100%;"
+              clearable>
+              <el-option
+                v-for="(item, index) in typeMap"
+                :key="index"
+                :label="item"
+                :value="index"/>
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="创建日期" prop="time_period">
+            <el-date-picker
+              v-model="form.time_period"
+              type="datetimerange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              style="width: 315px;">
+            </el-date-picker>
+          </el-form-item>
+        </div>
+
+        <el-button
+          slot="reference"
+          type="text">
+          <span>更多筛选</span>
+          <i class="el-icon-arrow-down cs-pl-5"/>
+        </el-button>
+      </el-popover>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  props: {
+    loading: {
+      default: false
+    },
+    typeMap: {
+      default: () => {}
+    },
+    statusMap: {
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      form: {
+        order_code: undefined,
+        account: undefined,
+        type: undefined,
+        status: undefined,
+        begin_time: undefined,
+        end_time: undefined,
+        time_period: null
+      }
+    }
+  },
+  methods: {
+    handleFormSubmit(isRestore = false) {
+      let form = {}
+      // eslint-disable-next-line no-unused-vars
+      for (const index in this.form) {
+        if (!this.form.hasOwnProperty(index)) {
+          continue
+        }
+
+        // 时间段处理
+        if (index === 'time_period') {
+          if (this.form[index] && this.form[index].length === 2) {
+            form['begin_time'] = this.form[index][0].toUTCString()
+            form['end_time'] = this.form[index][1].toUTCString()
+          }
+
+          continue
+        }
+
+        form[index] = this.form[index]
+      }
+
+      this.$emit('submit', form, isRestore)
+    },
+    handleFormReset() {
+      this.form.time_period = null
+      this.$refs.form.resetFields()
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .more-filter {
+    margin-bottom: -18px;
+  }
 
+  .more-filter >>> label {
+    width: 82px;
+  }
+
+  .more-filter >>> .el-form-item__content {
+    margin-left: 82px;
+  }
 </style>
--- src/views/order/service/index.vue
+++ src/views/order/service/index.vue
@@ -3,14 +3,16 @@
     <page-header
       slot="header"
       :loading="loading"
+      :type-map="typeMap"
+      :status-map="statusMap"
       @submit="handleSubmit"
       ref="header"/>
 
-    <page-main
-      :loading="loading"
-      :table-data="table"
-      @tabs="handleTabs"
-      @refresh="handleRefresh"/>
+<!--    <page-main-->
+<!--      :loading="loading"-->
+<!--      :table-data="table"-->
+<!--      @tabs="handleTabs"-->
+<!--      @refresh="handleRefresh"/>-->
 
     <page-footer
       slot="footer"
@@ -27,13 +29,28 @@ export default {
   name: 'order-service-list',
   components: {
     'PageHeader': () => import('./components/PageHeader'),
-    'PageMain': () => import('./components/PageMain'),
+    // 'PageMain': () => import('./components/PageMain'),
     'PageFooter': () => import('@/layout/header-aside/components/footer')
   },
   data() {
     return {
       loading: false,
       table: [],
+      typeMap: {
+        '0': '仅退款',
+        '1': '退货退款',
+        '2': '换货',
+        '3': '维修'
+      },
+      statusMap: {
+        '0': '待处理',
+        '1': '已同意',
+        '2': '已拒绝',
+        '3': '已寄件',
+        '4': '售后中',
+        '5': '已撤销',
+        '6': '已完成'
+      },
       page: {
         current: 1,
         size: 0,
@@ -44,6 +61,26 @@ export default {
   mounted() {
   },
   methods: {
+    // 刷新列表页面
+    handleRefresh(isTurning = false) {
+      if (isTurning) {
+        !(this.page.current - 1) || this.page.current--
+      }
+
+      this.$nextTick(() => {
+        this.$refs.header.handleFormSubmit()
+      })
+    },
+    // 分页变化改动
+    handlePaginationChange(val) {
+      this.page = val
+      this.$nextTick(() => {
+        this.$refs.header.handleFormSubmit()
+      })
+    },
+    // 提交查询请求
+    handleSubmit(form, isRestore = false) {
+    }
   }
 }
 </script>

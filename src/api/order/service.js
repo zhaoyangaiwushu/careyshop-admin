951084f6e1144ab082a544d6dd8a5607d70e6636
思维空间 <dnyz520@126.com>
2020-04-24 10:12:35 +0800

    所需接口添加完毕

--- src/api/order/service.js
+++ src/api/order/service.js
@@ -53,3 +53,135 @@ export function getOrderServiceList(data) {
     data
   })
 }
+
+/**
+ * 同意(接收)一个售后服务单
+ * @param {String} service_no
+ * @returns
+ */
+export function setOrderServiceAgree(service_no) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.agree'
+    },
+    data: {
+      service_no
+    }
+  })
+}
+
+/**
+ * 拒绝一个售后服务单
+ * @param {String} service_no
+ * @param {String} result
+ * @returns
+ */
+export function setOrderServiceRefused(service_no, result) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.refused'
+    },
+    data: {
+      service_no,
+      result
+    }
+  })
+}
+
+/**
+ * 设置退换货、维修商品是否寄还商家
+ * @param {String} service_no
+ * @param {Number} is_return
+ * @returns
+ */
+export function setOrderServiceSendback(service_no, is_return) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.sendback'
+    },
+    data: {
+      service_no,
+      is_return
+    }
+  })
+}
+
+/**
+ * 设置一个售后服务单状态为”售后中”
+ * @param {String} service_no
+ * @returns
+ */
+export function setOrderServiceAfter(service_no) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.after'
+    },
+    data: {
+      service_no
+    }
+  })
+}
+
+/**
+ * 撤销一个售后服务单
+ * @param {String} service_no
+ * @returns
+ */
+export function setOrderServiceCancel(service_no) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.cancel'
+    },
+    data: {
+      service_no
+    }
+  })
+}
+
+/**
+ * 完成一个售后服务单
+ * @param {String} service_no
+ * @returns
+ */
+export function setOrderServiceComplete(service_no) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.complete'
+    },
+    data: {
+      service_no
+    }
+  })
+}
+
+/**
+ * 客服对售后服务单添加备注(顾客不可见)
+ * @param {String} service_no
+ * @param {String} remark
+ * @returns
+ */
+export function setOrderServiceRemark(service_no, remark) {
+  return request({
+    url: '/v1/order_service',
+    method: 'post',
+    params: {
+      method: 'set.order.service.remark'
+    },
+    data: {
+      service_no,
+      remark
+    }
+  })
+}

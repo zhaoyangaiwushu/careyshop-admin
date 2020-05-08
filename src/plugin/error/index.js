687dfffa68043a9c64367eb751d677646a6c4697
思维空间 <dnyz520@126.com>
2020-05-08 23:57:34 +0800

    全局错误捕捉

--- src/plugin/error/index.js
+++ src/plugin/error/index.js
@@ -1,29 +1,58 @@
 import store from '@/store'
 import util from '@/utils/util'
+import { get, isObject } from 'lodash'
 
 export default {
   install(Vue, options) {
-    Vue.config.errorHandler = (error, instance, info) => {
-      Vue.nextTick(() => {
-        // store 追加 log
-        store.dispatch('careyshop/log/push', {
-          message: `${info}: ${error.message}`,
-          type: 'danger',
-          meta: {
-            error,
-            instance
-          }
-        })
+    function writeLog(logType) {
+      return (error, vm, info = '') => {
+        Vue.nextTick(() => {
+          store.dispatch('careyshop/log/push', {
+            message: `${info}: ${isObject(error) ? error.message : error}`,
+            type: logType,
+            meta: { error, vm }
+          })
 
-        // 只在开发模式下打印 log
-        if (process.env.NODE_ENV === 'development') {
-          util.log.capsule('CareyShop', 'ErrorHandler', 'danger')
+          if (process.env.NODE_ENV !== 'development') {
+            return
+          }
+
+          util.log.capsule('CareyShop', 'ErrorHandler', logType)
           util.log.danger('>>>>>> 错误信息 >>>>>>')
           console.log(info)
           util.log.danger('>>>>>> Vue 实例 >>>>>>')
-          console.log(instance)
+          console.log(vm)
           util.log.danger('>>>>>> Error >>>>>>')
           console.log(error)
+        })
+      }
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+      Vue.config.warnHandler = writeLog('warning')
+    }
+
+    Vue.config.errorHandler = writeLog('danger')
+    window.onunhandledrejection = error => {
+      store.dispatch('careyshop/log/push', {
+        message: get(error, 'reason.message', 'Unknown error'),
+        type: 'danger',
+        meta: {
+          error: get(error, 'reason'),
+          trace: get(error, 'reason.stack')
+        }
+      })
+    }
+
+    window.onerror = (event, source, lineno, colno, error) => {
+      store.dispatch('careyshop/log/push', {
+        message: get(error, 'message', 'Unknown error'),
+        type: 'danger',
+        meta: {
+          error,
+          trace: get(error, 'stack'),
+          source: `${source}@${lineno}:${colno}`,
+          event: event
         }
       })
     }

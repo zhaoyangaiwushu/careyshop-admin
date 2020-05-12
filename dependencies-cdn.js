312c1ecc6a704be93ff2acc21877c74e6866a9b7
思维空间 <dnyz520@126.com>
2020-05-12 14:19:34 +0800

    up

--- dependencies-cdn.js
+++ dependencies-cdn.js
@@ -17,7 +17,7 @@ module.exports = [
   {
     name: 'vuex',
     library: 'Vuex',
-    js: '//cdn.jsdelivr.net/npm/vuex@3.3.0/dist/vuex.min.js',
+    js: '//cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js',
     css: ''
   },
   {

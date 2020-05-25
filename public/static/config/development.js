713bbed74d212322a6bc745fb602d73f9ed89ca3
思维空间 <dnyz520@126.com>
2020-05-25 22:33:48 +0800

    up

--- public/static/config/development.js
+++ public/static/config/development.js
@@ -1,5 +1,5 @@
 const serverConfig = {
-  BASE_API: '//careyshop.oruei.com/index.php?s=api',
+  BASE_API: '/api',
   APP_KEY: '44936871',
   APP_SECRET: '43f50f75f4231a56b0221f2740414eb8'
 }
--- public/static/config/production.js
+++ public/static/config/production.js
@@ -1,5 +1,5 @@
 const serverConfig = {
-  BASE_API: '//demo.careyshop.cn/api',
+  BASE_API: '/api',
   APP_KEY: '86757125',
   APP_SECRET: 'ea1bd533d001fd73b09944f04c96a6fc'
 }
